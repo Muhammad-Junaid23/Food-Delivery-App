be20d5d7965fa3eba5e2702260303fb4b029e622
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-12-23 23:15:02 +0500

    added pages files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import styled, { ThemeProvider } from "styled-components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import {lightTheme} from './utils/Themes'
 
 const Container = styled.div``;
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
-     <h1>Welcome to Food delivery App</h1>
-    </ThemeProvider>
+     <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+      <Container>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+        </Routes>
+      </Container>
+      </BrowserRouter>
+     </ThemeProvider>
   );
 }
 
